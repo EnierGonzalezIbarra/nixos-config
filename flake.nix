b06cf07c8e07e357781b429e390e5a3ed3dac42b
Enier González Ibarra <79170796+EnierGonzalezIbarra@users.noreply.github.com>
2024-02-28 11:22:26 -0500

    Updated flake.nix to use home-manager once more and use 23.11's versions of pkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
@@ -35,14 +36,14 @@
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
-    # homeConfigurations = {
-    #   "enier@nixos" = home-manager.lib.homeManagerConfiguration {
-    #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-    #     extraSpecialArgs = {inherit inputs outputs;};
-    #
-    #     # > Our main home-manager configuration file <
-    #     modules = [./home-manager/home.nix];
-    #   };
-    # };
+     homeConfigurations = {
+       "enier@nixos" = home-manager.lib.homeManagerConfiguration {
+         pkgs = nixpkgs.legacyPackages.x86_64-linux;  Home-manager requires 'pkgs' instance
+         extraSpecialArgs = {inherit inputs outputs;};
+    
+          > Our main home-manager configuration file <
+         modules = [./home-manager/home.nix];
+       };
+     };
   };
 }
