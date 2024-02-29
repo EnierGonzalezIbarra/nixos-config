5f2295c9ad49cde3dde91f0a81aeca8164591f88
Enier González Ibarra <79170796+EnierGonzalezIbarra@users.noreply.github.com>
2024-02-29 05:09:34 +0000

    Updated state version for home-man

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
     # Shameless plug: looking for a way to nixify your themes and make
@@ -38,10 +38,10 @@
     # Available through 'home-manager --flake .#your-username@your-hostname'
      homeConfigurations = {
        "enier@nixos" = home-manager.lib.homeManagerConfiguration {
-         pkgs = nixpkgs.legacyPackages.x86_64-linux;  Home-manager requires 'pkgs' instance
+         pkgs = nixpkgs.legacyPackages.x86_64-linux;  #Home-manager requires 'pkgs' instance
          extraSpecialArgs = {inherit inputs outputs;};
     
-          > Our main home-manager configuration file <
+          # > Our main home-manager configuration file <
          modules = [./home-manager/home.nix];
        };
      };
