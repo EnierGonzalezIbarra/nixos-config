02250fd3f7163f21dd19956e1f6b498dc0a14082
Enier González Ibarra <79170796+EnierGonzalezIbarra@users.noreply.github.com>
2024-03-15 02:00:26 +0000

    removed blanks

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,11 +42,6 @@
     };
   };
 
-  # home = {
-  #   username = "enier";
-  #   homeDirectory = "/home/enier/";
-  # };
-
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   home.packages = with pkgs; [
