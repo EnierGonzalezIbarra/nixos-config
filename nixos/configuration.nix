54aaecceeeac0b3849d1c1499312c3bda8b9d842
Enier González Ibarra <79170796+EnierGonzalezIbarra@users.noreply.github.com>
2024-03-15 03:44:00 +0000

    Removed plasma

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -110,7 +110,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   
-  services.xserver.desktopManager.plasma6.enable = true;
+  # services.xserver.desktopManager.plasma6.enable = true;
 
   programs.hyprland = {
     enable = true;
@@ -190,6 +190,8 @@
     floorp
     gimp
     libsForQt5.kalk
+    libsForQt5.dolphin
+    libsForQt5.dolphin-plugins
     pandoc
     proxychains-ng
     thunderbird
