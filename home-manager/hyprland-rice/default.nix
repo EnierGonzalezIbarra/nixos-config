f0c7a0d28451e7df00760449570950d83472d0c0
Enier González Ibarra <79170796+EnierGonzalezIbarra@users.noreply.github.com>
2024-05-03 00:24:32 +0000

    Changed custom fontsize to 10

--- home-manager/hyprland-rice/default.nix
+++ home-manager/hyprland-rice/default.nix
@@ -3,7 +3,7 @@ let
   # Variables to share accross configs
   custom = {
     font = "RobotoMono Nerd Font";
-    fontsize = "12";
+    fontsize = "10";
     primary_accent = "cba6f7";
     secondary_accent= "89b4fa";
     tertiary_accent = "f5f5f5";
